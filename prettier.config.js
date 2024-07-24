e346d2508ea091398594197eda3c1cedd6c3559f
Forrest Jones <fjones-dev@8bitforest.com>
2024-07-23 23:23:26 -0600

    :tada: Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+const prettierConfig = {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'avoid',
+
+    plugins: ['prettier-plugin-svelte'],
+    pluginSearchDirs: ["."],
+    overrides: [{"files": "*.svelte", "options": {"parser": "svelte"}}]
+}
+
+export default prettierConfig
---
+++ src/app.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<main>
+    <span>Hello World</span>
+</main>
+
+<style>
+</style>
